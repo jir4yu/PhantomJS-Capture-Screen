7e7b6cc6da5c6ca4d350fad5b62bd6d970e58543
Jirayu Limjinda <jir4yu.l@gmail.com>
2017-12-30 11:27:28 +0700

    fixed more syntax

--- pt-capture-screen.js
+++ pt-capture-screen.js
@@ -1,41 +1,47 @@
-var page = require('webpage').create(),
-	system = require('system'),
-	t, address, pageSize = 0;
+var page = require('webpage').create();
+var fs = require('fs');
+var system = require('system');
+var t = 0;
+var address = 0;
+var pageSize = 0;
 
 /**
- * SET VIEWPORT HERE
+ * Set viewport size here
  */
 var viewports = {
-	'mobile': [375, 667],
-	'tablet': [768, 1024],
-	'desktop': [1366, 768]
+	mobile: [375, 667],
+	tablet: [768, 1024],
+	desktop: [1400, 900]
 };
 
-/**
- * TRIGGER WHEN GOT PAGE
- * CHECK PAGE STATUS AND PAGE SIZE
- */
+if (fs.makeDirectory('screenshots')) {
+	console.log('Directory was created');
+} else {
+	console.log('Directory exist or no permission to create.');
+}
+
 page.onLoadFinished = function(status) {
 	console.log('Status: ' + status);
 };
 
 page.onResourceReceived = function(response) {
 	if (response.bodySize !== undefined) {
-		pageSize += parseInt(response.bodySize, 10)
-	};
+		pageSize += parseInt(response.bodySize, 10);
+	}
 };
 
 /**
- * OPEN WEBPAGE TO CAPTURE SCREEN
- * CALCULATE PAGE SPEED AND PAGESIZE
+ * Open target website to capture and calculate pagespeed, pagesize
  */
 t = Date.now();
 address = system.args[1];
 page.open(address, function(status) {
 	var body = page.evaluate(function() {
-		// FORCE BACKGROUND COLOR TO WHITE
-		if (document.body.bgColor === '')
-			document.body.bgColor = 'white';
+		/**
+		 * Force window background to white,
+		 * useful when your body, document isn't filled wite white color
+		 */
+		if (document.body.bgColor === '') document.body.bgColor = 'white';
 		return document.body.bgColor;
 	});
 	if (status !== 'success') {
@@ -45,12 +51,17 @@ page.open(address, function(status) {
 		t = Date.now() - t;
 		console.log('Loading time: ' + t + ' msec');
 		console.log('Page size: ' + pageSize / 1000 + 'Kb');
-		// USE SETTIMEOUT TO VERIFIED 
-		// ALL CONTENT(ANIMATE EFFECT) WAS SUCCESSFUL LOADING.
+		/**
+		 * Use setTimeout to verify all animate content was loaded.
+		 * and scroll to bottom of page to get all dynamic content before make screenshot
+		 */
 		window.setTimeout(function() {
 			Object.keys(viewports).forEach(function(key) {
-				var w = viewports[key][0],
-					h = viewports[key][1];
+				var w = viewports[key][0];
+				var h = viewports[key][1];
+				page.evaluate(function() {
+					window.scrollTo(0, document.body.scrollHeight);
+				});
 				page.viewportSize = {
 					width: w,
 					height: h
@@ -61,12 +72,12 @@ page.open(address, function(status) {
 					width: w,
 					height: h
 				};
-				page.render(key + '.jpg', {
+				page.render('screenshots/' + key + '.jpg', {
 					format: 'jpg',
 					quality: 80
 				});
-			})
+			});
 			phantom.exit();
-		}, 1500);
+		}, 3200);
 	}
-})
+});
